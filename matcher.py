93d6cd735830bbae7a12751787ed7fabc390a59b
George Goranov <g.p.goranov@gmail.com>
2016-05-31 20:39:06 +0300

    remove enumeration and add a separate counter

--- matcher.py
+++ matcher.py
@@ -1,18 +1,23 @@
 input1 = open('input1.csv', 'r+')
 input2 = open('input2.csv', 'r+')
 result = open('result.csv', 'w')
+num1 = 0
 
 result.write('Supplier,Number of relations,list of lines\n')
 
-for num1, line1 in enumerate(input1, 1):
+for line1 in input1:
+    num1 += 1
     list_of_lines = []
     relations = 0
     input2.seek(0, 0)
-    for num2, line2 in enumerate(input2, 1):
+    num2 = 0
+    for line2 in input2:
+        num2 += 1
         if line1.strip() in line2:
             relations += 1
             list_of_lines.append(num2)
     if relations > 1:
+        print("Discovered match for " + line1.strip())
         result.write(
             line1.strip() +
             ',' + str(relations) +
