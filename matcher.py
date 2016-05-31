7c30bd720e2a843d96f89fcd263d19fa7a895140
George Goranov <g.p.goranov@gmail.com>
2016-05-31 21:24:50 +0300

    add files in lists

--- matcher.py
+++ matcher.py
@@ -1,21 +1,21 @@
-input1 = open('input1.csv', 'r')
-input2 = open('input2.csv', 'r')
+input1 = open('input1.csv', 'r').read().split('\n')
+input2 = open('input2.csv', 'r').read().split('\n')
+try:
+    input1.remove('')
+    input2.remove('')
+except ValueError:
+    print('file(s) do not have empty lines')
 result = open('result.csv', 'w')
-num1 = 0
 
 result.write('Supplier,Number of relations,list of lines\n')
 
 for line1 in input1:
-    num1 += 1
     list_of_lines = []
     relations = 0
-    input2.seek(0, 0)
-    num2 = 0
     for line2 in input2:
-        num2 += 1
         if line1.strip() in line2:
             relations += 1
-            list_of_lines.append(num2)
+            list_of_lines.append(input2.index(line2))
     if relations > 1:
         print("Discovered match for " + line1.strip())
         result.write(
