ac2aba6ae4ddd3260f7f107a43804c8ec472f56b
George Goranov <g.p.goranov@gmail.com>
2016-05-31 19:04:44 +0300

    read both files and compare

---
+++ matcher.py
@@ -0,0 +1,14 @@
+input1 = open('input1.csv', 'r+')
+input2 = open('input2.csv', 'r+')
+result = open('result.csv', 'w')
+
+result.write('Supplier,Number of relations,list of lines\n')
+
+for num1, line1 in enumerate(input1, 1):
+    list_of_lines = []
+    relations = 0
+    for num2, line2 in enumerate(input2, 1):
+        if line1.strip() in line2:
+            relations += 1
+            list_of_lines.append(num2)
+    result.write(line1.strip() + ',' + str(relations) + ',' + str(list_of_lines) + '\n')
