0c29f6d71f47351a788cd0a877a8b4823e639883
George Goranov <g.p.goranov@gmail.com>
2016-06-01 00:13:23 +0300

    various fixes

--- matcher.py
+++ matcher.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 
 input1 = open('input1.csv', 'r').read().split('\n')
@@ -8,9 +9,9 @@ lock = threading.Lock()
 number_of_threads = 8
 
 
-def calculate(suppliers):
+def calculate(start, end):
     global result
-    for line1 in suppliers:
+    for line1 in input1[start:end]:
         list_of_lines = []
         relations = 0
         current_line = 0
@@ -29,21 +30,20 @@ def calculate(suppliers):
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     try:
         input1.remove('')
         input2.remove('')
     except ValueError:
-        print('file(s) do not have empty lines')
+        print('Some file(s) do not have empty lines but that\'s ok.\
+        I can handle it')
     result.write('Supplier,Number of relations,list of lines\n')
-    threads = [threading.Thread(target=f, args=(
+    threads = [threading.Thread(target=calculate, args=(
         int(thread*len(input1)/number_of_threads),
         int((thread+1)*len(input1)/number_of_threads))) for
         thread in range(number_of_threads)]
     for t in threads:
         t.start()
-        print(t)
     for t in threads:
         t.join()
-
-    print('Threads finished')
-    print(result)
+    print("Executed in %s seconds" % (time.time() - start_time))
