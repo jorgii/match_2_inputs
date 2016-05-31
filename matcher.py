697608736272cec505505ca21171352caa8f3a3e
George Goranov <g.p.goranov@gmail.com>
2016-05-31 21:38:38 +0300

    set counter for better performance

--- matcher.py
+++ matcher.py
@@ -1,5 +1,5 @@
 input1 = open('input1.csv', 'r').read().split('\n')
-input2 = open('input2.csv', 'r').read().split('\n')
+input2 = open('input21.csv', 'r').read().split('\n')
 try:
     input1.remove('')
     input2.remove('')
@@ -12,10 +12,12 @@ result.write('Supplier,Number of relations,list of lines\n')
 for line1 in input1:
     list_of_lines = []
     relations = 0
+    current_line = 0
     for line2 in input2:
+        current_line += 1
         if line1.strip() in line2:
             relations += 1
-            list_of_lines.append(input2.index(line2))
+            list_of_lines.append(current_line)
     if relations > 1:
         print("Discovered match for " + line1.strip())
         result.write(
